5c2735ed9859b965cb55e9c6fdbb47827f28bd57
Dan Jones <good.evil.genius@gmail.com>
2015-02-23 16:24:56 -0500

    It works!

--- notification-pushbullet-class.php
+++ notification-pushbullet-class.php
@@ -2,7 +2,7 @@
 
 require_once(plugin_dir_path( __FILE__ ) . '/vendor/autoload.php');
 
-class DJones_Notification_Pushbullet extends NF_Notification_Base_Type {
+class NF_DJ_Notification_Pushbullet extends NF_Notification_Base_Type {
     function __construct() {
         $this->name = __( 'Pushbullet', 'ninja-pushbullet-notify' );
     }
@@ -19,30 +19,81 @@ class DJones_Notification_Pushbullet extends NF_Notification_Base_Type {
 
 		if ( $id == '' ) {
             $access_token = '';
-            $device = "all";
-            $title = "Form Submission";
-            $message = '';
-            $send_url = false;
-            $url = admin_url('edit.php?post_type=nf_sub');
+            $push_device = "";
+            $push_type = "note";
+            $push_title = "";
+            $push_url = '';
+            $push_message = '';
         } else {
             $access_token = Ninja_Forms()->notification( $id )->get_setting( 'access_token' );
-            $device = Ninja_Forms()->notification( $id )->get_setting( 'device' );
-            $title = Ninja_Forms()->notification( $id )->get_setting( 'title' );
-            $message = Ninja_Forms()->notification( $id )->get_setting( 'message' );
-            $send_url = Ninja_Forms()->notification( $id )->get_setting( 'send_url' );
-            $url = Ninja_Forms()->notification( $id )->get_setting( 'url' );
+            $push_message = Ninja_Forms()->notification( $id )->get_setting( 'push_message' );
+            $push_type = Ninja_Forms()->notification( $id )->get_setting( 'push_type' );
+            $push_title = Ninja_Forms()->notification( $id )->get_setting( 'push_title' );
+            $push_url = Ninja_Forms()->notification( $id )->get_setting( 'push_url' );
+            $push_device = Ninja_Forms()->notification( $id )->get_setting( 'push_device' );
         }
 
         $p = !empty($access_token) ? new Pushbullet($access_token) : false;
-        $devices = $p ? $p->getDevices()->devices : array();
+        $push_devices = $p ? $p->getDevices()->devices : array();
+        $pushtypes = array('note','link');
+
 
         $all_devices = new stdClass;
         $all_devices->nickname = 'All devices';
         $all_devices->iden = '';
-        array_unshift($devices, $all_devices);
+        array_unshift($push_devices, $all_devices);
 
         require(plugin_dir_path( __FILE__ ) . 'settings.tpl.php');
+
+		do_action( 'nf_dj_pushbullet_notification_after_settings', $id );
+    }
+
+	/**
+	 * Process our Push notification
+	 *
+	 * @access public
+	 * @since 2.8
+	 * @return void
+	 */
+	public function process( $id ) {
+		$access_token = Ninja_Forms()->notification( $id )->get_setting( 'access_token' );
+        if (empty($access_token)) {
+            file_put_contents('/tmp/notifications-pushbullet-class.log', "Empty access token\n", FILE_APPEND);
+            return;
+        }
+        $form_id = ( '' != $id ) ? Ninja_Forms()->notification( $id )->form_id : '';
+        $push_type = Ninja_Forms()->notification( $id )->get_setting( 'push_type' );
+        $push_title = $this->process_setting( $id, 'push_title' );
+        $push_url = Ninja_Forms()->notification( $id )->get_setting( 'push_url' );
+        $push_message = $this->process_setting( $id, 'push_message' );
+        $push_device = Ninja_Forms()->notification( $id )->get_setting( 'push_device' );
+        if (empty($push_url)) $push_url = admin_url('edit.php?post_status=all&post_type=nf_sub&form_id=' . $form_id);
+
+        $p = new Pushbullet($access_token);
+
+        try {
+            if ($push_type == "note") {
+                $p->pushNote($push_device, $push_title, $push_message);
+            } else if ($push_type == "link") {
+                $p->pushLink($push_device, $push_title, $push_url, $push_message);
+            }
+        } catch (PushbulletException $e) {
+            return;
+        }
+    }
+
+	/**
+	 * Explode our settings by ` and extract each value.
+	 * Check to see if the setting is a field; if it is, assign the value.
+	 * Run shortcodes and return the result.
+	 *
+	 * @access public
+	 * @since 2.8
+	 * @return array $setting
+	 */
+	public function process_setting( $id, $setting, $html = 1 ) {
+        return implode(' ', parent::process_setting($id, $setting, $html));
     }
 }
 
-return new DJones_Notification_Pushbullet();
+return new NF_DJ_Notification_Pushbullet();
--- settings.tpl.php
+++ settings.tpl.php
@@ -7,12 +7,48 @@
 </tr>
 
 <tr>
-  <th scope="row"><label for="settings-device"><?php _e('Device', 'ninja-pushbullet-notify'); ?></label></th>
+  <th scope="row"><label for="settings-push_device"><?php _e('Device', 'ninja-pushbullet-notify'); ?></label></th>
   <td>
-	<select name="settings[device]" id="settings-device">
-	  <?php foreach($devices as $dev): ?>
-	  <option value="<?= $dev->iden?>"><?= $dev->nickname?></option>
+	<select name="settings[push_device]" id="settings-push_device">
+	  <?php foreach($push_devices as $dev): ?>
+	  <?php $sel = ($push_device == $dev->iden); ?>
+	  <option value="<?= $dev->iden?>" <?= $sel?'selected="selected"':''?>><?= $dev->nickname?></option>
 	  <?php endforeach; ?>
 	</select>
+  </td>
+</tr>
+
+<tr>
+  <th scope="row"><?php _e('Push type', 'ninja-pushbullet-notify'); ?></th>
+  <td>
+	<?php foreach($pushtypes as $ptype): ?>
+	<?php $sel = ($ptype == $push_type); ?>
+	<p>
+	  <input type="radio" name="settings[push_type]" value="<?=$ptype?>" id="settings-push_type-<?=$ptype?>" 
+			 <?= $sel?'checked="checked"':''?>
+			 /> 
+	  <label for="settings-push_type-<?=$ptype?>"><?=ucfirst($ptype)?></label>
+	</p>
+	<?php endforeach; ?>
+  </td>
+</tr>
+
+<tr>
+  <th scope="row"><label for="settings-push_title">Title</label></th>
+  <td>
+	<input name="settings[push_title]" type="text" id="settings-push_title" value="<?php echo $push_title; ?>" class="nf-tokenize" placeholder="<?php _e( 'Push Title or search for a field', 'ninja-pushbullet-notify' ); ?>" data-token-limit="0" data-key="push_title" data-type="all" value="<?=$push_title?>" />
+	<span class="howto"><?php _e( 'This will be the title of the push.', 'ninja-pushbullet-notify' ); ?></span>
+  </td>
+</tr>
+
+<tr>
+  <th scope="row"><label for="settings-push_message"><?php _e( 'Body', 'ninja-pushbullet-notify' ); ?></label></th>
+  <td>
+	<?php
+	   $settings = array(
+	     'textarea_name' => 'settings[push_message]',
+	   );
+	   wp_editor( $push_message, 'push_message', $settings );
+	?>
   </td>
 </tr>
